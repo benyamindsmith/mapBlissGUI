b0da49a9715d5a1c1aa1217347737dfdc77c0c3d
bsmithClearSum <46410142+benyamindsmith@users.noreply.github.com>
2023-03-02 16:52:46 -0500

    starting to take shape

---
+++ app.R
@@ -0,0 +1,81 @@
+library(shiny)
+library(shinyBS)
+library(shinyWidgets)
+library(leaflet)
+library(mapBliss)
+
+ui <- fluidPage(
+    navbarPage("My Application",
+               tabPanel("Plot Flight Route",
+                        fluidRow(
+                          column(
+                            2,
+                            actionButton("addFlight", "Add Location"),
+                          ),
+                          column(10, 
+                                 actionButton("plotFlightRoute","Plot Flight Route"),
+                                 leafletOutput("flightRoutePlot"))
+                        )),
+               tabPanel("Plot Road Trip",
+                        sidebarPanel(
+                          actionButton("addFlight", "Add Location")
+                        ),
+                        mainPanel()),
+               tabPanel("Plot Hybrid Route",
+                        sidebarPanel(
+                          actionButton("addFlight", "Add Location")
+                        ),
+                        mainPanel()),
+               tabPanel("Plot City View",
+                        fluidRow(
+                          column(
+                            2,
+                            textInput("cityView","Type City Name")
+                            ),
+                        column(10, 
+                               actionButton("plotCityView","Plot Flight Route"),
+                               br(),
+                               br(),
+                               leafletOutput("cityViewPlot"))
+                        )
+               )
+              )
+)
+
+server <- function(input, output, session) {
+  
+  observeEvent(input$addFlight, {
+    insertUI(
+      selector = "#addFlight",
+      where = "afterEnd",
+      ui = list(
+      textInput(paste0("addFlight", input$addFlight),"",width="50%"),
+      actionButton(paste0("removeFlight",input$addFlight),
+                   label="",
+                   icon=icon("trash"),
+                   onclick=paste0('var $btn=$(this); 
+                                  setTimeout(function(){
+                                                        $btn.remove();
+                                                        document.getElementById("addFlight',input$addFlight,'").remove();
+                                                        },0);')
+      )
+    )
+    )
+  }
+  )
+  
+  observeEvent(input$plotFlightRoute,{
+    output$plotFlightRoute <- renderLeaflet({
+      
+    })
+  })
+  
+  
+  observeEvent(input$plotCityView,{
+    output$cityViewPlot <- renderLeaflet({
+      mapBliss::plot_city_view(input$cityView)
+    })
+  })
+}
+
+shinyApp(ui = ui, server = server)
